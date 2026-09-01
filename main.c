46f994898d33970241468b63ddf8b90461c148c5
lazarus shilongo <225020238@nust.na>
2026-09-01 13:44:38 +0200

    Week 1 - Municipal Financial Management System

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
-//slangu
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    char mayor[50];
+    int population;
+    printf("---------------------------Welcome to Windhoek Municipality---------------------------\n");
+    
+    printf("Enter municipal Name :\n");
+    scanf("%s",& name);
+
+    printf("enter the mayors name :\n");
+    scanf("%s",&mayor);
+    
+    printf("what is the population in whindhoek size :\n");
+    scanf("%d",&population);
+
+    //printf("hello %s",name);
+    printf("\nmayor name is : %s and the poulation in windhoek is : %d", mayor,population );
+    return 0;
+
+}
